42bceb836bd97089c7b329baff2afd047ffc5d03
tannirunaresh <65593535+tannirunaresh@users.noreply.github.com>
2025-07-20 13:19:22 +0530

    Add .next/static/chunks/src_app_chit-fund_page_tsx_6842b45d._.js 1752997761997

---
+++ .next/static/chunks/src_app_chit-fund_page_tsx_6842b45d._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_86ab5fb8._.js",
+  "static/chunks/node_modules_fe98b42d._.js"
+],
+    source: "dynamic"
+});
